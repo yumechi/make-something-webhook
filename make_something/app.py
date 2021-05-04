ac16f0afee5c7fd6f82cbfd98f7e73638a38bbf1
yumechi <yumechi0525@gmail.com>
2021-05-04 18:37:32 +0900

    test deploy

--- make_something/app.py
+++ make_something/app.py
@@ -1,6 +1,14 @@
 from chalice import Chalice
+import requests
 
 app = Chalice(app_name='make_something')
+
+def parse_backlog_model(data):
+    return {
+        "username": "uchia",
+        "content": "うげー。。。",
+    }
+
 
 @app.route('/kibela', methods=['POST'])
 def kibela_webhook():
@@ -11,5 +19,6 @@ def kibela_webhook():
 @app.route('/backlog', methods=['POST'])
 def backlog_webhook():
     body = app.current_request.json_body
-    print(body)
+    request_body = parse_backlog_model(body)
+    requests.post(DISCORD_URL, data=request_body)
     return {'mode': 'backlog'}
