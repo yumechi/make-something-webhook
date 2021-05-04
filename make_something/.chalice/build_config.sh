dc7ee4848fbf768f994a6b8089ccf3286f4f286e
yumechi <yumechi0525@gmail.com>
2021-05-04 18:54:31 +0900

    using kamidana(jinja2 cli) for env params

---
+++ make_something/.chalice/build_config.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+kamidana config.j2 --data config_env.json > config.json
--- make_something/app.py
+++ make_something/app.py
@@ -1,13 +1,23 @@
 from chalice import Chalice
+import os
+from typing import Dict, Any
 import requests
 
 app = Chalice(app_name='make_something')
 
 def parse_backlog_model(data):
+    # TODO: mock
     return {
         "username": "uchia",
         "content": "うげー。。。",
     }
+
+def post_content(url: str, body: Dict[str, Any]):
+    headers = {
+        'Content-Type' : 'application/json',
+        'User-Agent': 'Yumechi WebHook/1.0',
+    }
+    requests.post(url, data=body)
 
 
 @app.route('/kibela', methods=['POST'])
@@ -20,5 +30,7 @@ def kibela_webhook():
 def backlog_webhook():
     body = app.current_request.json_body
     request_body = parse_backlog_model(body)
-    requests.post(DISCORD_URL, data=request_body)
+
+    webhook_url = os.environ.get('WEBHOOK_URL')
+    post_content(webhook_url, request_body)
     return {'mode': 'backlog'}
